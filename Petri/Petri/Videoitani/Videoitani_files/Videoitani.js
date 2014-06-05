04603bf23cb81dccb9d5f9bbc5e554f0d03dd975
Petri Sirkkala <sirpete@iki.fi>
2014-06-05 11:45:48 +0300

    Edited "video" blog

--- Petri/Petri/Videoitani/Videoitani_files/Videoitani.js
+++ Petri/Petri/Videoitani/Videoitani_files/Videoitani.js
@@ -6,6 +6,6 @@ function photocastSubscribe()
 {photocastHelper("http://sirkkalap.github.io/Petri/Petri/Videoitani/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Videoitani_files/VideoitaniMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id6');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id6');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
